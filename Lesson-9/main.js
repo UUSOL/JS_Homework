08b4dfba30739a77cd3d1fb08dcbc1c3346fcd62
Uladzimir Soltan <soltanvladimir@yahoo.com>
2020-11-22 22:20:31 +0100

    Homework 9

---
+++ Lesson-9/main.js
@@ -0,0 +1,42 @@
+function Animal(name) {
+    var foodAmount = 50;
+
+    var self = this;
+
+    function formatFoodAmount() {
+        return foodAmount + ' гр.';
+    }
+
+    self.dailyNorm = function(amount) {
+        if (!arguments.length) return formatFoodAmount();
+
+        if (amount < 50 || amount > 500) {
+            return 'Недопустимое количество корма.';
+        }
+
+        foodAmount = amount;
+    };
+
+    this.name = name;
+
+    this.feed = function() {
+        console.log('Насыпаем в миску ' + self.dailyNorm() + ' корма.');
+    };
+}
+
+
+function Cat(name) {
+    Animal.apply(this, arguments);
+   
+   var animalFeed = this.feed;
+    this.feed = function() {
+        console.log(animalFeed());
+        console.log('Кот доволен ^_^');
+        return this;
+    }
+
+    this.stroke = function() {
+        console.log('Гладим кота.');
+        return this;
+    }
+}
